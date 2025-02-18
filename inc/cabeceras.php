44e8b27d59e8c59edbc04de5c04a2f52576758e2
Fernando Valdivielso <iamvaldidev@gmail.com>
2025-02-18 19:33:59 +0100

    wip

--- inc/cabeceras.php
+++ inc/cabeceras.php
@@ -1,19 +1,16 @@
 <?php
 
-// Este archivo carga las cabeceras de la tabla
 
-//include "utilidades/error.php";                           // Incluyo los mensajes de error
-include "config/config.php";                          // Traigo la conexión a la base de datos
+include "config/config.php";
 
-$peticion = "SHOW COLUMNS FROM " . $_GET['tabla'];	// Quiero todas las columnas de una tabla
-$resultado = $conexion->query($peticion);				// Ejecuto la consulta contra la base de datos
+$peticion = "SHOW COLUMNS FROM " . $_GET['tabla'];
+$resultado = $conexion->query($peticion);
 
-while ($fila = $resultado->fetch_assoc()) {			// Para cada resultado obtenido
-	echo "<td>" . $fila['Field'] . "</td>";					// Creo una columna de la tabla
+while ($fila = $resultado->fetch_assoc()) {
+	echo "<td>" . $fila['Field'] . "</td>";
 }
 echo "
 	<td>
 		
 	</td>";
-$conexion->close();											// Cierro la base de datos
-?>
+$conexion->close();
--- logout.php
+++ logout.php
@@ -1,10 +1,9 @@
 <?php
 /*
-	Este archivo se llama en el caso de ejecutar un cierre de sesión y es el encargado de 
-	en primer lugar iniciar la sesión a continuación destruir la sesión y 
-	por último redireccionar al formulario de inicio de sesión 
-	*/
+	Se encarga de cerrar la sesión y redirigir al usuario a la página de inicio.
+	Inicia la sesión para poder destruirla.
+*/
+
 session_start();
 session_destroy();
 header("Location: index.html");
-?>
