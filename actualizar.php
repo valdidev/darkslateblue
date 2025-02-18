7b1f99367175033765e3adefe3b252410a5542dd
Fernando Valdivielso <iamvaldidev@gmail.com>
2025-02-18 19:58:33 +0100

    comentario

--- actualizar.php
+++ actualizar.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-	Este script procesa la acción de actualización enviada desde el escritorio.
-	Se invoca mediante peticiones AJAX y recibe cuatro parámetros principales: 
-	- La tabla en la que se realizará la actualización.
-	- La columna que se modificará.
-	- El nuevo contenido que se asignará a la columna.
-	- El identificador que determina el registro específico a actualizar.
-	Con estos datos, el script construye y ejecuta una consulta SQL de tipo uPDATE en la base de datos.
+	Este script maneja las solicitudes de actualización enviadas desde el panel de administración.
+	Se ejecuta a través de peticiones AJAX y requiere cuatro parámetros clave:
+	- El nombre de la tabla donde se realizará la actualización.
+	- La columna que se desea modificar.
+	- El nuevo valor que se asignará a la columna.
+	- El identificador único del registro que se actualizará.
+	Con esta información, el script genera y ejecuta una consulta SQL de tipo uPDATE en la base de datos.
 */
 
 include "config/config.php";
